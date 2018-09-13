1072eef3237a1e5bc6f4d525f939d9c7a5921cc3
Unknown <20capel@elmbrookstudents.org>
2018-09-13 13:10:32 -0500

    First and last name no space

---
+++ src/outputExamples/FirstLastName.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Leah");
+System.out.println("Cape");
+	}
+
+}
