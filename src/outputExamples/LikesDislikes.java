09d0fd1a251074cd5c81adfe26270638172206cf
Unknown <20capel@elmbrookstudents.org>
2018-09-13 13:58:51 -0500

    Likes dislikes

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes \t \t Dislikes");
+		System.out.println("Reading \t Brusselsprouts");
+		System.out.println("Jazz \t \t Homework");
+	}
+
+}
