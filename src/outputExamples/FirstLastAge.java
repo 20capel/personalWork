ee8dcbbbfbf49c6befdfbf24728c2346cf2ed84f
Unknown <20capel@elmbrookstudents.org>
2018-09-13 13:18:14 -0500

    First and last name and age

---
+++ src/outputExamples/FirstLastAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstLastAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Leah");
+System.out.println("Cape");
+System.out.println("16");
+	}
+
+}
