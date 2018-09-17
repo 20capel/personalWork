b79837e8ac2998648183ad155ad870add652d956
Unknown <20capel@elmbrookstudents.org>
2018-09-17 14:05:02 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("L \t \t C \t \t C");
+System.out.println("L \t   C    C \t C    C");
+System.out.println("L);
+	}
+
+}
