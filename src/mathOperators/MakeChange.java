c0bd1bbf41930ee513775863a90f66074d78b5ec
Unknown <20capel@elmbrookstudents.org>
2018-09-25 15:09:59 -0500

    In Progress Make Change

---
+++ src/mathOperators/MakeChange.java
@@ -0,0 +1,25 @@
+package mathOperators;
+import java.util.Scanner;
+public class MakeChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double change=2.40;
+double dollars=2;
+int quarters=3;
+int dimes=1;
+int nickels=1;
+int pennies=2;
+Scanner input=new Scanner(System.in);
+System.out.println("How much change is needed?");
+change=input.nextDouble();
+dollars=change%1.0;
+quarters=(int)dollars%25;
+
+
+
+
+quarters=
+	}
+input.close();
+}
