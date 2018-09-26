aff58ca134b5eb11c08a0090e448910753f67a38
Unknown <20capel@elmbrookstudents.org>
2018-09-26 15:01:26 -0500

    Changes to Make Change

--- src/mathOperators/MakeChange.java
+++ src/mathOperators/MakeChange.java
@@ -6,20 +6,26 @@ public class MakeChange {
 		// TODO Auto-generated method stub
 double change=2.40;
 double dollars=2;
-int quarters=3;
-int dimes=1;
-int nickels=1;
-int pennies=2;
+double quarters=3;
+double dimes=1;
+double nickels=1;
+double pennies=2;
 Scanner input=new Scanner(System.in);
 System.out.println("How much change is needed?");
 change=input.nextDouble();
-dollars=change%1.0;
-quarters=(int)dollars%25;
+dollars=(int)change;
+change%=1;
+quarters=(change/0.25);
+change%=0.25;
+dimes=change/0.1;
+change%=0.1;
+nickels=change/0.05;
+change%=0.05;
+pennies=change/0.01;
+change%=0.01;
 
-
-
-
-quarters=
-	}
+System.out.println("You will need "+(int)dollars+" dollars, "+(int)quarters+" quarters, "+(int)dimes+" dimes, "+(int)nickels+" nickels, and "+(int)Math.ceil(pennies)+" pennies.");
 input.close();
+	}
+
 }
