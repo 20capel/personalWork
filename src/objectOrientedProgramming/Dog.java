565b6e02c9c710cdbd0ee66dd4bec8566e870c00
Unknown <20capel@elmbrookstudents.org>
2018-10-01 15:06:00 -0500

    Dog Declare

---
+++ src/objectOrientedProgramming/Dog.java
@@ -0,0 +1,16 @@
+package objectOrientedProgramming;
+
+public class Dog {
+
+	public Dog()
+	{
+	}
+public void sayWoof()
+{
+	System.out.println("Woof!");
+}
+public void dogAttack()
+{
+	System.out.println("Grrr");
+}
+}
