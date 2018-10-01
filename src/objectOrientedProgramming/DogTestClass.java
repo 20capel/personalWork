fbb63dc50c3ac460ae7f5c1304ed3fa01ce27f8e
Unknown <20capel@elmbrookstudents.org>
2018-10-01 15:06:09 -0500

    Dog Test Class

---
+++ src/objectOrientedProgramming/DogTestClass.java
@@ -0,0 +1,11 @@
+package objectOrientedProgramming;
+
+public class DogTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog dog = new Dog();
+		dog.dogAttack();
+	}
+
+}
