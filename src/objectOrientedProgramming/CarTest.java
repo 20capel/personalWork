962d114c56a8cffa446a7c9464b46cc0369dc973
Unknown <20capel@elmbrookstudents.org>
2018-10-02 15:07:50 -0500

    Car Test

---
+++ src/objectOrientedProgramming/CarTest.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car carNeeds = new Car();
+		carNeeds.sayOdometer();
+		carNeeds.sayFlat();
+		carNeeds.sayThirsty();
+	}
+
+}
