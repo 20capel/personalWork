a087b6a070e22389f1d987f14738f4cbc67fdd5c
Unknown <20capel@elmbrookstudents.org>
2018-10-02 13:58:57 -0500

    OOP Dog sayWoof dogAttack

--- src/objectOrientedProgramming/Dog.java
+++ src/objectOrientedProgramming/Dog.java
@@ -2,15 +2,14 @@ package objectOrientedProgramming;
 
 public class Dog {
 
-	public Dog()
-	{
+	public Dog() {
 	}
-public void sayWoof()
-{
-	System.out.println("Woof!");
-}
-public void dogAttack()
-{
-	System.out.println("Grrr");
-}
+
+	public void sayWoof() {
+		System.out.println("Woof!");
+	}
+
+	public void dogAttack() {
+		System.out.println("Grrr");
+	}
 }
