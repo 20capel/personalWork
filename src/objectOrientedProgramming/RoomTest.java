b3d235fc6d36583cea807adba914aa294adae87f
Unknown <20capel@elmbrookstudents.org>
2018-10-03 14:39:25 -0500

    Testing Room

---
+++ src/objectOrientedProgramming/RoomTest.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class RoomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Room wallArea = new Room();
+		wallArea.area();
+		Room newWallArea = new Room();
+		newWallArea.areaWithDoor();
+	}
+
+}
