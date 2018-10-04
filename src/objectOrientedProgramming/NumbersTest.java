5ef1ab46b9a93da0fb51bf95c35515cf325a1be6
Unknown <20capel@elmbrookstudents.org>
2018-10-04 14:12:11 -0500

    Numbers Test Class

---
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numbers lee = new Numbers();
+		lee.sayNumber(7.5);
+		lee.sayNumberPlus2(7.5);
+		lee.saySum(7.5,9.5);
+	}
+
+}
