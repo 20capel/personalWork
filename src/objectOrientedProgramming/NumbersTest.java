f1c65c1689e0f3775d566adc6bee16f271c84e50
Unknown <20capel@elmbrookstudents.org>
2018-10-05 15:09:36 -0500

    New Numbers Test

--- src/objectOrientedProgramming/NumbersTest.java
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -8,6 +8,15 @@ public class NumbersTest {
 		lee.sayNumber(7.5);
 		lee.sayNumberPlus2(7.5);
 		lee.saySum(7.5,9.5);
+		double thing=lee.returnSquare(8.0);
+		//8.0 squared (64) becomes stored as lee.returnSquare
+		//so you have to have the returnSquare declared as an object variable
+		//and then output that variable
+		System.out.println(thing);
+		double answer=lee.returnArea(5.0,4.0);
+		System.out.println(answer);
+		double round=lee.returnRoundUp(7.5);
+		System.out.println(round);
 	}
 
 }
