2e3c72a4f78743c25e9f270c00c17f7e2e144048
Unknown <20capel@elmbrookstudents.org>
2018-10-09 15:05:44 -0500

    Employee

--- src/objectOrientedProgramming/Employee.java
+++ src/objectOrientedProgramming/Employee.java
@@ -1,10 +1,22 @@
 package objectOrientedProgramming;
 
 public class Employee {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private double wage;
+	private int experience;
+public Employee(){
+	wage =20.0;
+	experience=0;
+}
+public double getWage(){
+	return wage;
+}
+public void setWage(double nowWage){
+	wage=nowWage;
 	}
-
+public int getExperience(){
+	return experience;
+}
+public void setExperience(int nowExperience){
+	experience=nowExperience;
+}
 }
--- src/objectOrientedProgramming/EmployeeTest.java
+++ src/objectOrientedProgramming/EmployeeTest.java
@@ -4,7 +4,13 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Employee bob=new Employee();
+System.out.println(bob.getWage());
+bob.setWage(67);
+System.out.println(bob.getWage());
+System.out.println(bob.getExperience());
+bob.setExperience(1);
+System.out.println(bob.getExperience());
 	}
 
 }
