6bf9da96c509e4519658806d178b19eb568c9c11
Unknown <20capel@elmbrookstudents.org>
2018-10-10 15:09:27 -0500

    Rectangle

---
+++ src/objectOrientedProgramming/Rectangle.java
@@ -0,0 +1,12 @@
+package objectOrientedProgramming;
+
+public class Rectangle {
+	private double length;
+	private double width;
+//default constructor below
+public Rectangle(){
+	length=9.0;
+	width=3.0;
+	}
+
+}
