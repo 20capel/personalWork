d48d76aad838e7c50e0f64918e8067eb4a000afe
Leah Cape <20capel@elmbrookstudents.org>
2018-10-16 14:39:48 -0500

    Worker Test

--- src/objectOrientedProgramming/WorkerTest.java
+++ src/objectOrientedProgramming/WorkerTest.java
@@ -8,7 +8,7 @@ Worker mark=new Worker(5,9.0);
 System.out.println("Your number of hours is "+mark.getHours());
 System.out.println("Your hourly rate is "+mark.getRate());
 System.out.println("Your check will be "+mark.payCheck());
-System.out.println("Congrats, you got a raise! You now get paid: "+mark.raise(9.0));
+
 	}
 
 }
