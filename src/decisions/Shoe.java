b37d31b0dc26eca3d8695da203808cc143214df2
Leah Cape <20capel@elmbrookstudents.org>
2018-10-17 14:03:47 -0500

    Shoe Boolean

--- src/decisions/Shoe.java
+++ src/decisions/Shoe.java
@@ -5,8 +5,8 @@ public class Shoe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner input=new Scanner(System.in);
-System.out.println("What is your height and shoe size?");
-double height=input.nextDouble();
+System.out.println("What is your height in inches and shoe size?");
+int height=input.nextInt();
 double shoeSize=input.nextDouble();
 System.out.println(height+shoeSize>=26.0);
 	}
