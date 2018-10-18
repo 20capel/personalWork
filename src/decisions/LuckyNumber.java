cbe6e121acb55a48cec48fdd295b0d94e94cc4b1
Leah Cape <20capel@elmbrookstudents.org>
2018-10-18 14:05:17 -0500

    Adult Kid Lucky Number Decisions

---
+++ src/decisions/LuckyNumber.java
@@ -0,0 +1,24 @@
+package decisions;
+import java.util.Scanner;
+public class LuckyNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+System.out.println("How old are you?");
+int age=input.nextInt();
+if(age>=18){
+	System.out.println("What is your height in inches?");
+	double height=input.nextDouble();
+	double luckyNumber=height*5;
+	System.out.println("Your lucky number is "+luckyNumber);
+}
+else{
+	System.out.println("What is your weight?");
+	double weight=input.nextDouble();
+	double luckyNumber=weight/2;
+	System.out.println("Your lucky number is "+luckyNumber);
+}
+	}
+
+}
