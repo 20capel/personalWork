6642f38e07125afcc4c2b3fc41147ecf46ae4d1c
Leah Cape <20capel@elmbrookstudents.org>
2018-10-19 15:05:15 -0500

    Fun Loops!!

---
+++ src/decisions/TestScores.java
@@ -0,0 +1,21 @@
+package decisions;
+import java.util.Scanner;
+public class TestScores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner poseidon=new Scanner(System.in);
+System.out.println("Enter three test scores:");
+double test1=poseidon.nextDouble();
+double test2=poseidon.nextDouble();
+double test3=poseidon.nextDouble();
+double average=(test1+test2+test3)/3;
+if(average>=92.5){
+	System.out.println("congrats you have an A");
+}
+else{
+	if(average)
+}
+	}
+
+}
