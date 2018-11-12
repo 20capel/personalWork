14b310911870201b1d38de326a6cdf0e38f127bf
Leah Cape <20capel@elmbrookstudents.org>
2018-11-12 14:44:01 -0600

    Display letter

---
+++ src/strings/DisplayLetter.java
@@ -0,0 +1,15 @@
+package strings;
+import java.util.Scanner;
+public class DisplayLetter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner gregory=new Scanner(System.in);
+System.out.println("Enter a sentence");
+String sentence=gregory.nextLine();
+System.out.println(sentence.charAt(0));
+System.out.println(sentence.charAt(2));
+System.out.println(sentence);
+	}
+
+}
