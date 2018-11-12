9e0e21902324cf5828c2b5537e2a3064ad1bc50d
Leah Cape <20capel@elmbrookstudents.org>
2018-11-12 15:04:26 -0600

    Contains word

---
+++ src/strings/Contains.java
@@ -0,0 +1,13 @@
+package strings;
+import java.util.Scanner;
+public class Contains {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner lilo=new Scanner(System.in);
+System.out.println("Enter a sentence");
+String sentence=lilo.nextLine();
+System.out.println(sentence.contains("bob"));
+	}
+
+}
