956ad1ef701b2c4491d193fe23c70b122bcea86f
Leah Cape <20capel@elmbrookstudents.org>
2018-11-12 14:21:18 -0600

    String and Yahtzee

---
+++ src/decisions/Strings.java
@@ -0,0 +1,15 @@
+package decisions;
+import java.util.Scanner;
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+		System.out.println("Enter your age");
+int age=input.nextInt();
+System.out.println("Enter your name");
+String name=input.nextLine();
+System.out.println(name+age);
+	}
+
+}
