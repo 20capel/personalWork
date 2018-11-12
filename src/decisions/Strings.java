d8f0fe09d967d5faecaafc30bc67e5c63179d963
Leah Cape <20capel@elmbrookstudents.org>
2018-11-12 14:37:28 -0600

    Strings

--- src/decisions/Strings.java
+++ src/decisions/Strings.java
@@ -1,15 +1,18 @@
 package decisions;
+
 import java.util.Scanner;
+
 public class Strings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input=new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter your age");
-int age=input.nextInt();
-System.out.println("Enter your name");
-String name=input.nextLine();
-System.out.println(name+age);
+		int age = input.nextInt();
+		input.nextLine();
+		System.out.println("Enter your name");
+		String name = input.nextLine();
+		System.out.println(name + " "+age);
 	}
 
 }
