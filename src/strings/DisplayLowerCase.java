21ea09e72d1d3bd7caa2e6c77142ff3960cc470c
Leah Cape <20capel@elmbrookstudents.org>
2018-11-13 15:05:36 -0600

    Display Lower Case and Number Characters

---
+++ src/strings/DisplayLowerCase.java
@@ -0,0 +1,14 @@
+package strings;
+import java.util.Scanner;
+public class DisplayLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner maria=new Scanner(System.in);
+System.out.println("Enter a sentence");
+String sentence=maria.nextLine();
+System.out.println(sentence.toLowerCase());
+System.out.println(sentence);
+	}
+
+}
--- src/strings/NumberOfCharacters.java
+++ src/strings/NumberOfCharacters.java
@@ -5,9 +5,17 @@ public class NumberOfCharacters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner milly=new Scanner(System.in);
-System.out.println("Enter a sentence");
+/*System.out.println("Enter a sentence");
 String sentence=milly.nextLine();
 System.out.println(sentence.length());
+int length=sentence.length();
+int counter=0;
+for(counter=0;counter<=(length-1);counter++){
+	System.out.println(sentence.charAt(counter));
+}*/
+String text="My name is Buzz Lightyear!";
+for(int i=0; i<text.length();i++){
+	System.out.println(text.substring(i));
+}
 	}
-
 }
