0ee025dfd7c9ecbd593650fe10c0088351103dc7
Leah Cape <20capel@elmbrookstudents.org>
2018-11-13 14:31:20 -0600

    First Letter and Num of Characters

---
+++ src/strings/FirstLetter.java
@@ -0,0 +1,13 @@
+package strings;
+import java.util.Scanner;
+public class FirstLetter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner jose=new Scanner(System.in);
+System.out.println("Enter a sentence");
+String sentence=jose.nextLine();
+System.out.println(sentence.indexOf("X"));
+	}
+
+}
---
+++ src/strings/NumberOfCharacters.java
@@ -0,0 +1,13 @@
+package strings;
+import java.util.Scanner;
+public class NumberOfCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner milly=new Scanner(System.in);
+System.out.println("Enter a sentence");
+String sentence=milly.nextLine();
+System.out.println(sentence.length());
+	}
+
+}
