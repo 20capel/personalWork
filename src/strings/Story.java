4353aed3de1254999c9655cfa8b0d0905fe2326e
Leah Cape <20capel@elmbrookstudents.org>
2018-11-19 14:58:45 -0600

    Story

--- src/strings/Story.java
+++ src/strings/Story.java
@@ -1,5 +1,35 @@
 package strings;
 
 public class Story {
+	private String story;
 
+	public Story(String newStory) {
+		story = newStory;
+	}
+
+	public String getStory() {
+		return story;
+	}
+
+	public int wordCount() {
+		int counter = 0;
+		int numSpaces = 1;
+		for (counter = 0; counter < story.length(); counter++) {
+			if (story.charAt(counter) == ' ') {
+				numSpaces++;
+			}
+		}
+		return numSpaces;
+	}
+
+	public String firstWord() {
+		String firstWord = story.substring(0, story.indexOf(" "));
+		return firstWord;
+	}
+	public int find() {
+		int counter=0;
+		for(counter=0;counter<story.length();counter++) {
+			if
+		}
+	}
 }
