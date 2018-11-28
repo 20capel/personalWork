d0a241ba507bbc9697d1beb23151a2287bafed97
Leah Cape <20capel@elmbrookstudents.org>
2018-11-28 15:09:37 -0600

    Arrays

--- src/strings/HundredRectanglesTest.java
+++ src/strings/HundredRectanglesTest.java
@@ -3,16 +3,20 @@ import java.util.Random;
 public class HundredRectanglesTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		// TODO Auto-generated method
 Random generator=new Random();
 Rectangle[] rectangle=new Rectangle[100];
-
+int smallest=rectangle[0].area();
+int randomNum=10+generator.nextInt(10+56-1);
 for(int counter=0;counter<rectangle.length;counter++){
-	int randomNum=10+generator.nextInt(10+56-1);
 	Rectangle hundred=new Rectangle(randomNum,randomNum);
 	rectangle[counter]=hundred;
-	System.out.println("Length: "+hundred.getLength()+"Width: "+hundred.getWidth());
+	if(hundred.area()<smallest){
+		smallest=hundred.area();
+	}
+	
 }
+System.out.println("Length: "+hundred.getLength()+"Width: "+hundred.getWidth());
 	}
 
 }
---
+++ src/strings/RollDieArrays.java
@@ -0,0 +1,11 @@
+package strings;
+import java.util.Random;
+import java.util.Scanner;
+public class RollDieArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
