cc672c93498c59b219060835bce3f63de2c49815
Leah Cape <20capel@elmbrookstudents.org>
2019-01-11 15:09:17 -0600

    Inheritance

--- src/strings/RectangleClass.java
+++ src/strings/RectangleClass.java
@@ -1,8 +1,14 @@
 package strings;
 
-public class RectangleClass extends Quadrilateral{
+public class RectangleClass extends Parallelogram implements Geo{
 public RectangleClass(){
-	Quadrilateral bob=new Quadrilateral();
-	bob.setSides(2.0,3.0,4.5,6.0);
+	super();
+}
+public RectangleClass(double length,double width){
+	super(length,width,width);
+	//*or super.setSides(length,width,length,width);*//
+}
+public double area(){
+	return super.getS1()*super.getS2();
 }
 }
