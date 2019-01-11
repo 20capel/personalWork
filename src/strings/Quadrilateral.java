24160444faec79c290125956f35cb4c0231d5b94
Leah Cape <20capel@elmbrookstudents.org>
2019-01-11 13:49:04 -0600

    Quadrilateral

--- src/strings/Quadrilateral.java
+++ src/strings/Quadrilateral.java
@@ -1,5 +1,42 @@
 package strings;
 
 public class Quadrilateral {
+private double s1;
+private double s2;
+private double s3;
+private double s4;
 
+public Quadrilateral(){
+	s1=2.0;
+	s2=3.0;
+	s3=4.0;
+	s4=5.0;
 }
+public Quadrilateral(double side1,double side2,double side3,double side4){
+	s1=side1;
+	s2=side2;
+	s3=side3;
+	s4=side4;
+}
+public void setSides(double side1,double side2,double side3,double side4){
+	s1=side1;
+	s2=side2;
+	s3=side3;
+	s4=side4;
+}
+public double getS1(){
+	return s1;
+}
+public double getS2(){
+	return s2;
+}
+public double getS3(){
+	return s3;
+}
+public double getS4(){
+	return s4;
+}
+public double perimeter(){
+	double perimeter=s1+s2+s3+s4;
+	return perimeter;
+}}
